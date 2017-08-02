ea054a2215d5dd2c1a61267a09badbc29308fdb9
Filip Ziętkowski <filip.zietkowski@gmail.com>
2017-08-02 17:32:59 +0200

    Created the base for ExchangeRatesSource test.

--- src/test/java/com/gmail/zietkowski/filip/exchangeratessource/ExchangeRatesSourceTest.java
+++ src/test/java/com/gmail/zietkowski/filip/exchangeratessource/ExchangeRatesSourceTest.java
@@ -1,10 +1,25 @@
 package com.gmail.zietkowski.filip.exchangeratessource;
 
+import org.junit.Before;
+
 /**
  * Test class for the ExchangeRatesSource implementation.
  * 
  * @author Filip Ziętkowski
  */
 public class ExchangeRatesSourceTest {
+    /**
+     * The tested class.
+     */
+    private ExchangeRatesSource exchangeRatesSource;
+    
+    /**
+     * The set up method, executed before every test.
+     */
+    @Before
+    public void before() {
+        exchangeRatesSource = new ExchangeRatesSourceImpl();
+    }
+    
     
 }
